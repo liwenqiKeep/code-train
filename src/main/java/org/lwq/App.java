cb43738bc8fa080a62ee79fb2c009402875bd84c
LiwenqiKeep <278956314@qq.com>
2023-06-29 16:38:39 +0800

    project init

---
+++ src/main/java/org/lwq/App.java
@@ -0,0 +1,13 @@
+package org.lwq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
